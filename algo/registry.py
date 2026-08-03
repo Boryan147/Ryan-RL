67aef6a4fe490a6f86c3ccd50c21cd515cc2529f
Ryan Li <bo.ryanli0420@gmail.com>
2026-08-03 15:38:27 +0200

    create agent registry for management

---
+++ algo/registry.py
@@ -0,0 +1,18 @@
+from .DQN.agent import DQNAgent
+from .DQN.Double_DQN.doubledqn_agent import Double_DQNAgent
+from .DQN.Dueling_DQN.dueling_agent import Dueling_DQNAgent
+from .REINFORCE.agent import REINFORCEAgent
+
+# register all the possible algorithm agents
+Agent_Registry = {
+    'DQN': DQNAgent,
+    'DoubleDQN': Double_DQNAgent,
+    'DuelingDQN': Dueling_DQNAgent,
+    'REINFORCE': REINFORCEAgent
+}
+
+# instantiate agent
+def make_agent(algo, obs_size, act_size, config):
+    if algo not in Agent_Registry:
+        print(f'Algorithm {algo} not found. Choose from {list(Agent_Registry.keys())}')
+    return Agent_Registry[algo](obs_size, act_size, config)
