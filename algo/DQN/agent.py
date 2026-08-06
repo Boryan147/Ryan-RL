ee96955598e60b9098288369f4d895adbc1c202e
Ryan Li <bo.ryanli0420@gmail.com>
2026-08-06 10:01:43 +0200

    chore

--- algo/DQN/agent.py
+++ algo/DQN/agent.py
@@ -86,7 +86,6 @@ class DQNAgent:
 
     # agent training loop
     def learn(self, env, writer=None):
-        # train_cfg = self.config['training'] 
         episode_reward = []
         global_step = 0
 
@@ -122,8 +121,8 @@ class DQNAgent:
                         epi_length = info['episode']['l']
                         episode_reward.append(epi_return)
                         if writer:
-                            writer.add_scalar('episodic reward', epi_return, global_step)
-                            writer.add_scalar('episodic length', epi_length, global_step)
+                            writer.add_scalar('charts/episodic reward', epi_return, global_step)
+                            writer.add_scalar('charts/episodic length', epi_length, global_step)
                     break
             if episode % 50 == 0 and episode_reward:
                 avg_rew = np.mean(episode_reward[-50:])
