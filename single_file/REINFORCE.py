615c9f06ac5632528c1460cbe72a84aea1199503
Ryan Li <bo.ryanli0420@gmail.com>
2026-08-26 18:09:05 +0200

    add single-file REINFORCE algo

--- single_file/REINFORCE.py
+++ single_file/REINFORCE.py
@@ -2,7 +2,6 @@ import argparse
 import os 
 import random
 import numpy as np
-import time
 from torch.utils.tensorboard import SummaryWriter
 import gymnasium as gym
 
@@ -28,17 +27,80 @@ class Agent(nn.Module):
         )
 
     def get_action(self, x):
-        return self.policynet(x)
+        logits = self.policynet(x)
+        probs = Categorical(logits=logits)
+        act = probs.sample()
+        return act, probs.log_prob(act)
 
     def get_value(self, x):
         return self.valuenet(x)
 
+def reward_to_go(rewards, gamma):
+    returns = np.zeros_like(rewards)
+    len_epi = len(rewards)
+    for i in reversed(range(len_epi)):
+        returns[i] = rewards[i] + (gamma * rewards[i+1] if i+1 < len_epi else 0)
+    return returns
+
+def gae(returns, lamb):
+    
+
 if __name__ == "__main__":
-    # set seed
+    # set the seed
+    random.seed(15)
+    np.random.seed(15)
+    torch.manual_seed(15)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(15) 
+    torch.backends.cudnn.deterministic = True
 
-    env = gym.make('CartPole-v1')
+    device = torch.device(
+        'cuda' if torch.cuda.is_available() else
+        'mps' if torch.backends.mps.is_available() else
+        'cpu'
+    )
+
+    env = gym.make('CartPole-v1', render_mode='rgb_array')
     env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = gym.wrappers.RecordVideo(env, '../videos', episode_trigger=lambda t: t % 100 == 0)
+    env.reset(15)
+    agent = Agent(env.observation_space.shape[0], env.action_space.n).to(device)
 
-    agent = Agent(env.observation_space.shape[0], env.action_space.n)
+    # hyperparameters
+    gamma = 0.99
+    lamb = 0.95
+    num_epi = 600
+    num_traj = 5
+
+    states, logprobs, rewards, values = [], [], [], []
+    # training loop
+    for i in range(num_epi):
+        obs, info = env.reset()
+        obs = torch.tensor(obs, dtype=torch.float32).to(device)
+        done = False
+
+        while not done:
+            act, logprob = agent.get_action(obs)
+            next_obs, reward, terminated, truncated, info = env.step(act.item())
+            next_obs = torch.tensor(next_obs, dtype=torch.float32).to(device)
+            done = terminated or truncated
+            value = agent.get_value(obs)
+
+            states.append(obs)
+            rewards.append(reward)
+            logprobs.append(logprob)
+            values.append(value)
+
+            obs = next_obs
+
+        if i == num_epi:
+
+
+
+
+    env.close()
+
+
+
 
 
