713208cf35f5f29ed1510d74ac322815cc62067a
Ryan Li <bo.ryanli0420@gmail.com>
2026-08-29 15:42:13 +0200

    reinforce V6

--- single_file/REINFORCE.py
+++ single_file/REINFORCE.py
@@ -24,11 +24,12 @@ class Agent(nn.Module):
             nn.Linear(64, 1)
         )
 
-    def get_action(self, x):
+    def get_action_and_value(self, x, act=None):
         logits = self.policynet(x)
         probs = Categorical(logits=logits)
-        act = probs.sample()
-        return act, probs.log_prob(act), probs.entropy()
+        if act is None:
+            act = probs.sample()
+        return act, probs.log_prob(act), probs.entropy(), self.valuenet(x)
 
     def get_value(self, x):
         return self.valuenet(x)
@@ -70,96 +71,92 @@ if __name__ == "__main__":
 
     env = gym.make('CartPole-v1', render_mode='rgb_array')
     env = gym.wrappers.RecordEpisodeStatistics(env)
-    env = gym.wrappers.RecordVideo(env, f'videos/{run_name}', episode_trigger=lambda t: t % 1000 == 0)
+    env = gym.wrappers.RecordVideo(env, f'videos/{run_name}', episode_trigger=lambda t: t % 50 == 0)
     agent = Agent(env.observation_space.shape[0], env.action_space.n).to(device)
 
     # hyperparameters
     gamma = 0.99
     gae_lambda = 0.97
-    lr = 3e-3
+    po_lr = 5e-3
+    v_lr = 3e-3
     num_epoches = 100
     num_steps = 1000
 
-    optimizer = optim.Adam(agent.policynet.parameters(), lr=5e-3)
-    optimizer_value = optim.Adam(agent.valuenet.parameters(), lr=3e-3)
-    global_step = 0
+    optimizer = optim.Adam(agent.policynet.parameters(), lr=po_lr)
+    optimizer_value = optim.Adam(agent.valuenet.parameters(), lr=v_lr)
 
     obs, info = env.reset(seed=seed)
     obs = torch.tensor(obs, dtype=torch.float32).to(device).unsqueeze(0) # shape (1, obs_size)
     done = False
+    global_step = 0
+
     # training loop
     for epoch in range(num_epoches):
-        b_states, b_logprobs, b_vtarget, b_adv = [], [], [], [] 
-        states, logprobs, rewards, values = [], [], [], []
+        # storage buffer setup
+        obs_buf = torch.zeros((num_steps, env.observation_space.shape[0]), device=device)
+        act_buf = torch.zeros(num_steps, device=device)
+        rew_buf = np.zeros(num_steps, dtype=np.float32)
+        val_buf = np.zeros(num_steps, dtype=np.float32)
+        adv_buf = np.zeros(num_steps, dtype=np.float32)
+        ret_buf = np.zeros(num_steps, dtype=np.float32)
+        start_idx = 0
 
-        for step in range(num_steps):
-            if done:
-                obs, info = env.reset()
-                obs = torch.tensor(obs, dtype=torch.float32).to(device).unsqueeze(0)
-            global_step += 1
-            act, logprob, _ = agent.get_action(obs)
-            next_obs, reward, terminated, truncated, info = env.step(act.item())
-            next_obs = torch.tensor(next_obs, dtype=torch.float32).to(device).unsqueeze(0)
-            done = terminated or truncated
-            value = agent.get_value(obs).item() # shape (1, 1)
+        with torch.no_grad():
+            for step in range(num_steps):
+                if done:
+                    obs, info = env.reset()
+                    obs = torch.tensor(obs, dtype=torch.float32).to(device).unsqueeze(0)
+                global_step += 1
+                act, logprob, _, value = agent.get_action_and_value(obs)
+                next_obs, reward, terminated, truncated, info = env.step(act.item())
+                next_obs = torch.tensor(next_obs, dtype=torch.float32).to(device).unsqueeze(0)
+                done = terminated or truncated
 
-            states.append(obs)
-            rewards.append(reward)
-            logprobs.append(logprob)
-            values.append(value)
+                obs_buf[step] = obs
+                act_buf[step] = act
+                rew_buf[step] = reward
+                val_buf[step] = value.item()
 
-            obs = next_obs
+                obs = next_obs
+                if done or step == num_steps - 1: 
+                    last_v = 0.0 if terminated else agent.get_value(obs).item()
+                        
+                    slice_ = slice(start_idx, step + 1)
+                    # compute reward-to-go & gae and populate buffer
+                    ret_buf[slice_] = reward_to_go(rew_buf[slice_], last_v, gamma)
+                    adv_buf[slice_] = gae(rew_buf[slice_], val_buf[slice_], last_v, gamma, gae_lambda)
 
-            # log performance metrics
-            if 'episode' in info:
-                epi_return = info['episode']['r']
-                epi_length = info['episode']['l']
-                writer.add_scalar('charts/episodic reward', epi_return, global_step)
-                writer.add_scalar('charts/episodic length', epi_length, global_step)
+                    start_idx = step + 1
 
-            if done or step == num_steps - 1: 
-                if terminated:
-                    last_v = 0
-                elif truncated or step == num_steps - 1:
-                    last_v = agent.get_value(obs).item()
+                # log performance metrics
+                if 'episode' in info:
+                    epi_return = info['episode']['r']
+                    epi_length = info['episode']['l']
+                    writer.add_scalar('charts/episodic reward', epi_return, global_step)
+                    writer.add_scalar('charts/episodic length', epi_length, global_step)
 
-                # compute episodic reward-to-go & gae
-                v_target = reward_to_go(rewards, last_v, gamma)
-                advantages = gae(rewards, values, last_v, gamma, gae_lambda)
-
-                # populate batches
-                b_states += states
-                b_logprobs += logprobs
-                b_vtarget += list(v_target)
-                b_adv += list(advantages)
-
-                # empty the episodic lists
-                states, logprobs, rewards, values = [], [], [], [] 
-
-        b_states = torch.cat(b_states)  # shape (num_steps, obs_size)
-        b_adv = torch.tensor(b_adv, dtype=torch.float32, device=device).detach() # shape (num_steps, )
-        b_logprobs = torch.cat(b_logprobs)  # shape (num_steps, )
-        b_vtarget = torch.tensor(b_vtarget, dtype=torch.float32, device=device)  # shape (num_steps, )
-        _, _, entropy = agent.get_action(b_states)
+        _, logp_buf, entropy, _ = agent.get_action_and_value(obs_buf, act=act_buf)
         entropy_loss = entropy.mean()
 
         # advantage normalization
+        b_adv = torch.from_numpy(adv_buf).to(device)
         b_adv = (b_adv - b_adv.mean()) / (b_adv.std() + 1e-8)
 
         # estimate policy gradient & update policy
-        pg_loss = -(b_logprobs * b_adv).mean()
+        pg_loss = -(logp_buf * b_adv).mean()
         optimizer.zero_grad()
         pg_loss.backward()
         optimizer.step()
 
         # define MSE loss for value function & update value
+        b_ret = torch.from_numpy(ret_buf).to(device)
         v_criterion = nn.MSELoss()
         # run multiple times to update value per epoch
         for _ in range(40):
-            v_pred = agent.get_value(b_states).view(-1)
+            v_pred = agent.get_value(obs_buf).view(-1)
             # explained variance of value function
-            explained_var = 1 - (b_vtarget - v_pred).var() / (b_vtarget.var() + 1e-8)
-            v_loss = v_criterion(v_pred, b_vtarget)
+            explained_var = 1 - (b_ret - v_pred).var() / (b_ret.var() + 1e-8)
+            v_loss = v_criterion(v_pred, b_ret)
             optimizer_value.zero_grad()
             v_loss.backward()
             optimizer_value.step()
